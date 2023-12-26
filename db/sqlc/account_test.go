e01a5629d4aab2046f1388db14ed885b72e73717
Mojtaba Michael <mojimich2015@gmail.com>
2023-12-26 21:16:31 +0300

    Added more tests

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,19 +3,26 @@ package db
 import (
 	"context"
 	"testing"
-	"github.com/stretchr/testify/require"
+	"time"
+
 	"github.com/mjmichael73/go-simple-bank/util"
+	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) (Account, CreateAccountParams) {
 	arg := CreateAccountParams{
-		Owner: util.RandomOwner(),
-		Balance: util.RandomMoney(),
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
 	require.NoError(t, err)
+	return account, arg
+}
+
+func TestCreateAccount(t *testing.T) {
+	account, arg := createRandomAccount(t)
 	require.NotEmpty(t, account)
 
 	require.Equal(t, arg.Owner, account.Owner)
@@ -24,4 +31,18 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1, _ := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
